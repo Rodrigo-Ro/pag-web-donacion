50b823e6cebd9558bf428b9c56bede43aac5e50c
Rodrigo-Ro <rodrigorodrigueztorres@gmail.com>
2026-06-09 17:22:32 -0500

    Creacion De Componentes
    Creacion de componentes y estilos para la pagina web

--- Pagina_web/src/app/app.ts
+++ Pagina_web/src/app/app.ts
@@ -1,12 +1,32 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+
+import { Navbar } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
+import { Stats } from './components/stats/stats';
+import { WhyDonate } from './components/why-donate/why-donate';
+import { Problematica } from './components/problematica/problematica';
+import { Campanas } from './components/campanas/campanas';
+import { Transparencia } from './components/transparencia/transparencia';
+import { Testimonios } from './components/testimonios/testimonios';
+import { Contacto } from './components/contacto/contacto';
+import { Cta } from './components/cta/cta';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    Navbar,
+    Hero,
+    Stats,
+    WhyDonate,
+    Problematica,
+    Campanas,
+    Transparencia,
+    Testimonios,
+    Contacto,
+    Cta
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('Pagina_web');
-}
+export class App {}
---
+++ Pagina_web/src/app/components/transparencia/transparencia.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transparencia',
+  standalone: true,
+  imports: [],
+  templateUrl: './transparencia.html',
+  styleUrl: './transparencia.css',
+})
+export class Transparencia {}
