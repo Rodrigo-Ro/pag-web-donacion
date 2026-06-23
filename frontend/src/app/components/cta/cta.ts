dd02de6c6cd26a7408849ac9a0840d0a7d2f6c0f
Rodrigo-Ro <rodrigorodrigueztorres@gmail.com>
2026-06-23 16:38:17 -0500

    Actualizacion de error de diseño
    Actualizacion de boton y direccion del boton

--- frontend/src/app/components/cta/cta.ts
+++ frontend/src/app/components/cta/cta.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cta',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cta.html',
   styleUrl: './cta.css',
 })
