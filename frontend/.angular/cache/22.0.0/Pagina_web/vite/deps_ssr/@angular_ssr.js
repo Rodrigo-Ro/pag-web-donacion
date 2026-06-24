e613f41c5603a16574c539b8eed7c0f10e85b480
CieloAzulAnglasRuiz <61012118@continental.edu.pe>
2026-06-24 00:01:50 -0500

    actualizacion del frontend

---
+++ frontend/.angular/cache/22.0.0/Pagina_web/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,43 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-7N3M535U.js";
+import "./chunk-LICEWOAT.js";
+import "./chunk-AYTOXRGP.js";
+import "./chunk-YX4UPHY3.js";
+import "./chunk-N4MJ7ACU.js";
+import "./chunk-4WNZRY3Q.js";
+import "./chunk-W6LG2RKB.js";
+import "./chunk-OPY43WTJ.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
